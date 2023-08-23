29f31ecdda57da95bcf8c78c19c6a4842bf82e21
JuseoKo <mariat1717@daum.net>
2023-08-24 06:20:10 +0900

    refactor def refactor

--- Game/main.py
+++ Game/main.py
@@ -11,8 +11,11 @@ def spacial_text_preprocessing(text_list: list):
     return text_list
 
 def text_word_parser(all_text: str):
+
     parser_text = re.sub('<img(.*?)\n', '\n', all_text)
-    # parser_text = re.sub('=(.*?)\n', '\n', parser_text)
+    # if \n
+    parser_text = re.sub('if (.*?)\n', '\n', parser_text)
+    parser_text = re.sub('=iif(.*?),', '\n', parser_text)
     parser_text = re.sub('<video(.*?)\n', '\n', parser_text)
     parser_text = re.sub('dynamic(.*?)\n', '\n', parser_text)
     parser_text = re.sub('(.*?)\':\n', '\n', parser_text)
@@ -39,47 +42,45 @@ def text_word_parser(all_text: str):
     text_list = spacial_text_preprocessing(text_list=text_list)
     # 2. 글자수로 정렬
     text_list = sorted(text_list, key=len, reverse=True)
+    return text_list
 
+def translator_run(text_list: list):
     # 번역
     text_dict = {}
     for text in text_list:
-        try:
-            text_dict[text] = trans_from(text)
-        except:
-            time.sleep(10)
-            text_dict[text] = trans_from(text)
+        trans_check = 1
+        while trans_check == 1:
+            try:
+                text_dict[text] = trans_from(text)
+            except:
+                time.sleep(10)
+                text_dict[text] = trans_from(text)
+            trans_check = 0
     #저장
-    with open('./Dict/game_dict.txt', 'w') as f:
-        f.write(str(json.dumps(text_dict, indent=4)))
-
-    return text_list
-
-
-def open_game_text(path: str):
-    with open(path, 'r', encoding='utf-16le') as f:
-        text_list = text_word_parser(all_text=f.read())
-
-
-def naver_translator(text: str):
-    import requests
-    client_id = "JXA1VR9b2RPkOqQJSw5A" # <-- client_id 기입
-    client_secret = "QJflmuVEc3" # <-- client_secret 기입
-    payload = {'text' : text,
-            'source' : 'en',
-            'target': 'ko'}
-    url = "https://openapi.naver.com/v1/papago/n2mt"
-    header = {"X-Naver-Client-Id":client_id,
-              "X-Naver-Client-Secret":client_secret}
-    response = requests.post(url, headers=header, data=payload).json()['message']['result']['translatedText']
-    return response
-
-def google_translator(text: str):
-    translated = str(translator.translate(text, src='en', dest='ko').text)
-    return translated
+    with open('./Dict/game_dict.py', 'w', encoding='utf-8') as f:
+        f.write('trans_dict = '+str(json.dumps(text_dict, indent=4, ensure_ascii=False)))
+    return None
 
 def trans_from(text: str):
+    """
+    text를 받아서 번역하는 코드입니다.
+    :param text:
+    :return:
+    """
     print(f'번역 전 : {text}')
-    # 변수 체크 및 저장
+    # 0. tag 변수 체크 및 저장
+    tag_pattern = r"<a(.*?)>|</a>|<font(.*?)>|</font>|<b>|</b>|<br>|</br>"
+    tag_check = re.findall(tag_pattern, text)
+    tag_dict = {}
+    # 중복제거
+    tag_check = [param[0] for param in list(set(tag_check))]
+    # tag 변환
+    for i in range(len(tag_check)):
+        tag_dict[tag_check[i]] = '#'*i
+    for k, v in tag_dict.items():
+        text = text.replace(k, v)
+
+    # 0. 변수 체크 및 저장
     param_check = re.findall('(?P<placeholder>(?:<+|<<)(.*?)(?:>+|>>))', text)
     param_dict = {}
     # 중복제거
@@ -90,19 +91,44 @@ def trans_from(text: str):
     for k, v in param_dict.items():
         text = text.replace(k, v)
     # 번역
-    translated = google_translator(text=text)
+    translated = str(translator.translate(text, src='en', dest='ko').text)
 
     # 변수 복구
     for k, v in param_dict.items():
+        translated = translated.replace(v, k)
+        translated = re.sub(v, k, translated, flags=re.IGNORECASE)
+    # tag 복구
+    for k, v in tag_dict.items():
         translated = translated.replace(v, k)
         translated = re.sub(v, k, translated, flags=re.IGNORECASE)
     print(f'번역 후 : {translated}')
     time.sleep(0.5)
     return translated
 
+def text_replace(path: str):
+    """
+    변환
+    :param path:
+    :return:
+    """
+    from Game.Dict.text.game_dict import dict_game
+    with open(path, 'r', encoding='utf-16le') as f:
+        text = f.read()
+        for k, v in dict_game.items():
+            text = text.replace(k, v)
+        #저장
+    with open(path, 'w', encoding='utf-16le') as f:
+        f.write(text)
 
 if __name__ == '__main__':
-    # 번역 객체 생성
+    # 0. 번역 객체 생성
     translator = Translator()
-    # google_translator(text=test)
-    open_game_text(path='./text.txt')
+    # 1. 파일 오픈 및 텍스트 파싱
+    path = './start.txt'
+    with open(path, 'r', encoding='utf-16le') as f:
+        text_list = text_word_parser(all_text=f.read())
+    # 2. 텍스트 번역 후 저장
+    translator_run(text_list=text_list)
+
+    # 별도. 게임 번역 후 저장
+    # text_replace(path=path)
